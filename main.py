9b3fe8e66988f3bc30d5bee4f225cec77576184a
Eray <eraay95@gmail.com>
2026-04-10 22:16:32 +0200

    create Store class

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from products import Product
+# from products import Product
 
-bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-mac = Product("MacBook Air M2", price=1450, quantity=100)
+# bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+# mac = Product("MacBook Air M2", price=1450, quantity=100)
 
-print(bose.buy(50))
-print(mac.buy(100))
-print(mac.is_active())
+# print(bose.buy(50))
+# print(mac.buy(100))
+# print(mac.is_active())
 
-bose.show()
-mac.show()
+# bose.show()
+# mac.show()
 
-bose.set_quantity(1000)
-bose.show()
+# bose.set_quantity(1000)
+# bose.show()
+
+import products
+from store import Store
+
+
+product_list = [
+    products.Product("MacBook Air M2", price=1450, quantity=100),
+    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+    products.Product("Google Pixel 7", price=500, quantity=250),
+]
+
+best_buy = Store(product_list)
+products_list = best_buy.get_all_products()
+
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products_list[0], 1), (products_list[1], 2)]))
